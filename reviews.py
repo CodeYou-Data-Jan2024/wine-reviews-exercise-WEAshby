152e60bc22ef7f570e32ad2ba0a0e0a62ebf20b7
william-ashby <weashby01@gmail.com>
2024-09-22 14:19:52 -0400

    added code

--- reviews.py
+++ reviews.py
@@ -1,2 +1,11 @@
-# add your code here
+import pandas as pd
+
+reviews=pd.read_csv(r"C:\Users\william.ashby\github-classroom\CodeYou-Data-Jan2024\wine-reviews-exercise-WEAshby\data\winemag-data-130k-v2.csv.zip",compression='zip')
+
+summary=reviews.groupby('country').points.agg(['count',('points','mean')]).sort_values('count',ascending=False)
+
+summary['points']=summary.points.round(1)
+print(summary)
+
+summary.to_csv(r'C:\Users\william.ashby\github-classroom\CodeYou-Data-Jan2024\wine-reviews-exercise-WEAshby\data\wine_reviews_per_country.csv', index=False)
 
